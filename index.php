aa57b9a6aa1d681f1273178c91d5acaced27977f
= <=>
2020-02-06 23:29:03 +0700

    vinh

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ define('LARAVEL_START', microtime(true));
 */
 
 require __DIR__.'/lib/vendor/autoload.php';
+require __DIR__.'/lib/vendor/autoload.php vinh';
 
 /*
 |--------------------------------------------------------------------------
